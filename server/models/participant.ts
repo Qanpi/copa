c2318da803e65d760e358c3bf1fcfbe44cf8bde1
Qanpi <aleksei.terin@outlook.com>
2023-09-17 22:59:59 +0300

    updated to use brackets schema from brackets-mongo-db implementation

--- server/models/participant.ts
+++ server/models/participant.ts
@@ -1,52 +1,49 @@
 import mongoose, { InferSchemaType } from "mongoose";
 import { collections } from "../configs/db.config.js";
 import { ObjectId } from "mongodb";
+import BracketsParticipantSchema from "brackets-mongo-db/dist/models/ParticipantSchema";
 
-const ParticipantSchema = new mongoose.Schema(
+const BracketsParticipant = mongoose.model(
+  "Participant",
+  BracketsParticipantSchema
+);
+
+const CopaParticipantSchema = new mongoose.Schema(
   {
-    group: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: collections.groups.id,
-    },
-
-    name: String, //TODO: consider what happens when you change name midtournament
+    // tournament: {
+    //   type: mongoose.SchemaTypes.ObjectId,
+    //   ref: collections.tournaments.id,
+    //   // index: true,
+    // },
     team: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: collections.teams.id,
-    },
-
-    division: {
-      type: String,
     },
     //TODO: make this more secure
     phoneNumber: {
       type: String,
       select: false,
     },
-    //TODO: index  this and team fields
-    tournament: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: collections.tournaments.id,
-      alias: "tournament_id",
-      index: true,
-    },
   },
   {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
+    id: true,
+    virtuals: {
+      createdAt: {
+        get() {
+          return this._id.getTimestamp();
+        },
+      },
+    },
+    toJSON: { virtuals: true, getters: true },
+    toObject: { virtuals: true, getters: true },
   }
 );
 
-type ParticipantVirtuals = {
-  createdAt: Date;
-  id: ObjectId;
-};
+const CopaParticipant = BracketsParticipant.discriminator(
+  "CopaParticipant",
+  CopaParticipantSchema
+);
 
-ParticipantSchema.virtual("createdAt").get(function () {
-  return this._id.getTimestamp();
-});
+export type TParticipant = InferSchemaType<typeof CopaParticipant.schema>;
 
-export type Participant = InferSchemaType<typeof ParticipantSchema> &
-  ParticipantVirtuals;
-
-export default mongoose.model(collections.participants.id, ParticipantSchema);
+export default CopaParticipant;
