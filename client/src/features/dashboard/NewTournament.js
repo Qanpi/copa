1f6d42425b72cf7dea23c3e8e26ef0d518769739
Qanpi <aleksei.terin@outlook.com>
2023-09-26 21:11:16 +0300

    simplified the page structure

--- client/src/features/dashboard/NewTournament.js
+++ client/src/features/dashboard/NewTournament.js
@@ -3,7 +3,8 @@ import {
   Container,
   Step,
   StepButton,
-  Stepper
+  Stepper,
+  Typography,
 } from "@mui/material";
 import { useQueryClient } from "@tanstack/react-query";
 import { Form, Formik } from "formik";
@@ -16,87 +17,11 @@ import MyTextField from "../inputs/mytextfield.js";
 import MyAutocomplete from "../inputs/MyAutocomplete.tsx";
 import { createTournament, useCreateTournament } from "../tournament/hooks.ts";
 
-const Parameters = () => {
-  return (
-    <>
-      <MyNumberSlider
-        label="Match length"
-        name="settings.matchLength"
-        units="mins"
-        min={1}
-        max={20}
-        step={1}
-        marks={[
-          { value: 1, label: "1 min" },
-          { value: 6, label: "6 min" },
-          { value: 20, label: "20 min" },
-        ]}
-      />
-      <MyNumberSlider
-        label="Player count"
-        name="settings.playerCount"
-        units="players"
-        required
-        min={1}
-        max={11}
-        step={1}
-        marks={[
-          { value: 1, label: "1 p" },
-          { value: 4, label: "4 p" },
-          { value: 11, label: "11 p" },
-        ]}
-      />
-      <MyAutocomplete name="divisions"/>
-    </>
-  );
-};
-
-const Rules = () => {
-  return <MyFileInput label="please enter rules" name="rules" />;
-};
-
-const ContactInfo = () => {
-  return (
-    <>
-      <MyTextField
-        type="text"
-        name="organizer.name"
-        label="Organizer's name"
-      ></MyTextField>
-      <MyTextField
-        type="tel"
-        name="organizer.phoneNumber"
-        label="Phone number"
-      ></MyTextField>
-    </>
-  );
-};
-
 function NewTournamentPage() {
   const createTournament = useCreateTournament();
 
-  const [activeStep, setActiveStep] = useState(0);
-  const steps = {
-    Parameters: Parameters,
-    Rules: Rules,
-    "Contact Info": ContactInfo,
-  };
-  const lastStep = Object.keys(steps).length - 1;
-
-  const handleClickStep = (i) => {
-    setActiveStep(i);
-  };
-
   return (
     <>
-      <Stepper activeStep={activeStep}>
-        {Object.keys(steps).map((label, i) => (
-          <Step key={label}>
-            <StepButton onClick={() => handleClickStep(i)}>{label}</StepButton>
-          </Step>
-        ))}
-      </Stepper>
-
       <Formik
         initialValues={{
           settings: {
@@ -120,31 +45,63 @@ function NewTournamentPage() {
             name: Yup.string().required(),
             phoneNumber: Yup.string().notRequired(),
           }),
-          rules: Yup.string().required(), //TODO: at lfor time being
+          rules: Yup.string(), 
         })}
         onSubmit={(values) => {
           createTournament.mutate(values);
         }}
       >
         <Form>
-          {/* Bit of a hack to ensure that the file input doesn't remount and as such reset */}
-          {Object.values(steps).map((Section, i) => {
-            return (
-              <Container
-                key={i}
-                sx={{ display: activeStep === i ? undefined : "none" }}
-              >
-                <Section></Section>
-              </Container>
-            );
-          })}
-          {activeStep === lastStep ? (
-            <Button type="submit">Submit</Button>
-          ) : (
-            <Button type="button" onClick={() => setActiveStep((s) => s + 1)}>
-              Next
-            </Button>
-          )}
+          <Typography variant="h3">KICKSTART COPA</Typography>
+          <Typography>
+            You can always modify these settings later on.
+          </Typography>
+
+          <MyNumberSlider
+            label="Match length"
+            name="settings.matchLength"
+            units="mins"
+            min={1}
+            max={20}
+            step={1}
+            marks={[
+              { value: 1, label: "1 min" },
+              { value: 6, label: "6 min" },
+              { value: 20, label: "20 min" },
+            ]}
+          />
+          <MyNumberSlider
+            label="Player count"
+            name="settings.playerCount"
+            units="players"
+            required
+            min={1}
+            max={11}
+            step={1}
+            marks={[
+              { value: 1, label: "1 p" },
+              { value: 4, label: "4 p" },
+              { value: 11, label: "11 p" },
+            ]}
+          />
+          <MyAutocomplete name="divisions" />
+          <MyTextField
+            type="text"
+            name="rules"
+            label="Link to rules"
+          ></MyTextField>
+          <MyTextField
+            type="text"
+            name="organizer.name"
+            label="Organizer's name"
+          ></MyTextField>
+          <MyTextField
+            type="tel"
+            name="organizer.phoneNumber"
+            label="Phone number"
+          ></MyTextField>
+
+          <Button type="submit">Submit</Button>
         </Form>
       </Formik>
     </>
