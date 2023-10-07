69b08d54b927e1d82b35e49875b483939b118fdc
Qanpi <aleksei.terin@outlook.com>
2023-10-07 12:29:57 +0300

    cleaned up team profile page and added isManager flag

--- client/src/features/team/profile/Team.js
+++ client/src/features/team/profile/Team.js
@@ -14,12 +14,16 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
 import { useParams } from "react-router-dom";
 import { useTeam } from "../hooks.ts";
+import { useUser } from "../../user/hooks.ts";
 
 dayjs.extend(relativeTime);
 
 function TeamPage() {
   const { name } = useParams();
+
+  const {data: user} = useUser("me");
   const {data: team, status: teamStatus} = useTeam(name);
+
 
   const {
     status: inviteStatus,
@@ -50,11 +54,12 @@ function TeamPage() {
     return <div>loadgi team profiel</div>;
   }
 
+  const isManager = user?.id === team?.manager;
+
   return (
     <>
       <h1>{team.name}</h1>
-      <h2>{team.manager}</h2>
-      <Button onClick={() => fetchInvite()}>Invite</Button>
+      {isManager ? <Button onClick={() => fetchInvite()}>Invite</Button> : null}
       {inviteStatus === "success" ? (
         <Alert>
           <AlertTitle>Generated invite link!</AlertTitle>
