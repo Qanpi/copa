29cbc566b17ce587e4f37a842a627972519f39a0
Qanpi <aleksei.terin@outlook.com>
2023-10-08 10:42:51 +0300

    init division model

---
+++ server/models/division.ts
@@ -0,0 +1,31 @@
+import mongoose, { SchemaTypes } from "mongoose";
+
+const DivisionSchema = new mongoose.Schema({
+    rules: {
+        type: String,
+    },
+    registration: {
+        from: Date,
+        to: Date,
+    },
+    settings: {
+        matchLength: {
+            type: Number,
+            default: 6,
+        },
+        playerCount: {
+            type: Number,
+            default: 4,
+        },
+    },
+    state: {
+        type: String,
+        enum: ["Kickstart", "Registration", "Group stage", "Bracket", "Complete"],
+        default: "Registration",
+    },
+}, {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+});
+
+export default mongoose.model("Division", DivisionSchema);
