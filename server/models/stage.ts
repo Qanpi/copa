5b460a63ad13aa8efc95fe2aa7b688b881f16e5b
Qanpi <aleksei.terin@outlook.com>
2023-10-09 23:25:16 +0300

    save tournament id to stage in pre-save middleware
    to be able to query stages by tournment

--- server/models/stage.ts
+++ server/models/stage.ts
@@ -1,9 +1,22 @@
 import { Stage } from "brackets-mongo-db";
-import mongoose from "mongoose";
+import mongoose, { SchemaTypes } from "mongoose";
+import Tournament from "./tournament.js";
+import Metadata from "./metadata.js";
 
-export default Stage.discriminator("Stage",
-    new mongoose.Schema({
-    }, {
-        _id: true
-    })
-);
+const StageSchema = new mongoose.Schema({
+    tournament: SchemaTypes.ObjectId
+}, {
+    _id: true
+})
+
+//TODO: test fo robustness
+StageSchema.pre("save", async function () {
+    if (this.isNew) {
+        const metadata = await Metadata.findOne({
+            model: Tournament.modelName
+        });
+        this.tournament = metadata?.latest;
+    }
+})
+
+export default Stage.discriminator("Stage", StageSchema);
