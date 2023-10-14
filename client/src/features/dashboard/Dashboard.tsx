7536c2af9bbd4b66313b4c60b948edf3ee99df82
Qanpi <aleksei.terin@outlook.com>
2023-10-14 15:31:37 +0300

    changed to ts
    take advantage of tournament state type

---
+++ client/src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,71 @@
+import {
+  Step,
+  StepButton,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
+import NewTournamentPage from "./NewTournament.js";
+import RegistrationStage from "./Registration.js";
+import { useTournament, useUpdateTournament } from "../viewer/hooks.ts";
+import GroupStage from "./GroupStage.js";
+import BracketStructure from "./BracketStructure.tsx";
+import Bracket from "./Bracket.tsx";
+import CreateTournamentPage from "./CreateTournament.tsx";
+
+function DashboardPage() {
+  const { data: tournament } = useTournament("current");
+  const updateTournament = useUpdateTournament(tournament?.id);
+
+  if (!tournament?.id) return <CreateTournamentPage></CreateTournamentPage>;
+
+  const currentSection = (): any => {
+    switch (tournament.state) {
+      case "Registration":
+        return (
+          <RegistrationStage
+            next={nextSection}
+            prev={prevSection}
+          ></RegistrationStage>
+        );
+      case "Groups":
+        return <GroupStage next={nextSection} prev={prevSection} ></GroupStage>;
+      case "Bracket":
+        return <Bracket next={nextSection} prev={prevSection} ></Bracket>;
+      case "Complete":
+        return <>COngrats! You've completed {tournament.name}</>
+      default:
+        return <Typography>Unknown tournament state.</Typography>;
+    }
+  };
+
+  const stateId = tournament.states.indexOf(tournament.state);
+
+  const nextSection = () => {
+    const next = tournament.states[stateId + 1];
+    updateTournament.mutate({ state: next });
+  };
+
+  const prevSection = () => {
+    const prev = tournament.states[stateId - 1];
+    updateTournament.mutate({ state: prev });
+  };
+
+  return (
+    <>
+      <Stepper activeStep={stateId} orientation="vertical" >
+        {
+          tournament.states.map((s, i) => (
+            <Step key={i} >
+              <StepLabel>{s} </StepLabel>
+            </Step>
+          ))
+        }
+      </Stepper>
+
+      {currentSection()}
+    </>
+  );
+}
+
+export default DashboardPage;
