cacd38c409af65a6bee0a352044796534cbceeaf
Qanpi <aleksei.terin@outlook.com>
2023-10-21 22:03:32 +0300

    normalizing margins and paddings

--- client/src/features/layout/NotFoundPage.tsx
+++ client/src/features/layout/NotFoundPage.tsx
@@ -6,7 +6,7 @@ function NotFoundPage() {
     const theme = useTheme();
 
     return (
-        <PromptContainer sx={{mt:10}}>
+        <PromptContainer>
             <Typography variant="h2" color={alpha(theme.palette.primary.main, 0.5)} > 404 NOT FOUND</Typography>
             <Typography variant="body1" textAlign={"left"}>Sorry, we went looking for this page but couldn't find who asked.</Typography>
         </PromptContainer> 
