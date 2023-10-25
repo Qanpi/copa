df29cd98af80806a1475349f08a280c482942e09
Qanpi <aleksei.terin@outlook.com>
2023-10-25 23:11:52 +0300

    prettier loading sate for grid

--- client/src/features/participant/ParticipantsTable.tsx
+++ client/src/features/participant/ParticipantsTable.tsx
@@ -43,7 +43,7 @@ const ParticipantCard = ({ name, banner, ...props }: CardProps & { name: string,
 }
 
 function TeamsPage() {
-  const { data: tournament } =
+  const { data: tournament, isLoading: isTournamentLoading } =
     useTournament("current");
 
   const division = useContext(DivisionContext);
@@ -71,14 +71,14 @@ function TeamsPage() {
             }}>
 
               {isLoading ? <>
-                {Array.from({ length: 10 }, (_, i) => <Skeleton variant="rounded" key={i} sx={{ width: "100%", height: "200px" }}></Skeleton>)}
+                {Array.from({ length: 20 }, (_, i) => <Skeleton variant="rounded" key={i} sx={{ width: "100%", height: "200px" }}></Skeleton>)}
               </> : null}
               {participants?.map(p => <ParticipantCard name={p.name} banner={p?.bannerUrl}></ParticipantCard>)}
             </Box>
           </Box> :
-            <PromptContainer>
-              <Typography>Registration hasn't begun yet.</Typography>
-            </PromptContainer>
+            <PromptContainer sx={{minHeight: "60vh"}}>
+              <Typography>{isTournamentLoading ? "" : "Registration hasn't begun yet"}</Typography>
+            </PromptContainer> 
           }
         </DivisionPanel>
       </Stack>
