eb654f40bc4fd36ddb644e989a6b872b33a2d449
Qanpi <aleksei.terin@outlook.com>
2023-10-31 20:18:25 +0200

    added cookie secret to env
    sample one for test to run in ci

--- server/app.ts
+++ server/app.ts
@@ -27,12 +27,12 @@ if (app.get("env") !== "production") {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.GOOGLE_CLIENT_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(
   cookieSession({
     name: "session",
-    secret: process.env.GOOGLE_CLIENT_SECRET,
+    secret: process.env.COOKIE_SECRET,
     maxAge: 24 * 60 * 60 * 1000 * 30, // 1 month
   })
 );
