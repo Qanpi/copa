85a07c3b57e3e74dbccefc6da1c7d5e1f653a5c9
Qanpi <aleksei.terin@outlook.com>
2023-11-01 14:55:48 +0200

    increased body json limit to avoid http 413

--- server/app.ts
+++ server/app.ts
@@ -25,7 +25,7 @@ if (app.get("env") !== "production") {
   app.use(logger("dev"));
 }
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
